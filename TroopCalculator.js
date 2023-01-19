277ef216fb422b193963cbbdfcf9e5ee2546ea51
PatrickHacault <patrickhacault893@gmail.com>
2023-01-19 00:05:05 -0600

    changed script src

--- TroopCalculator.js
+++ TroopCalculator.js
@@ -1,11 +1,15 @@
-let playerTroops = 30;
-let territories = 18;
-let systemValues = [];
-let systemTroops = 0;
+export default function troopCalculate(playerTroops, territories, systemValues, systemTroops) {
+   
 
-for(let i = 0; i < systemValues.length; i++) {
-    systemTroops += systemValues[i];
+    for(let i = 0; i < systemValues.length; i++) {
+        systemTroops += systemValues[i];
+    }
+    
+    playerTroops += ((Math.floor(territories/3))+systemTroops+3);
+    console.log(playerTroops);
+    
 }
 
-playerTroops += ((Math.floor(territories/3))+systemTroops+3);
-console.log(playerTroops);
+
+
+
