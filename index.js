a04562be0e4fe27a3bad350efff53d329fdd903f
PatrickHacault <patrickhacault893@gmail.com>
2023-01-19 00:04:32 -0600

    added troop calculate

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 //This contains the runtime information + functions + methods
-import {hello} from './TroopCalculator.js'
+import troopCalculate from './TroopCalculator.js';
 
-hello()
+let playerTroops = 30;
+let territories = 18;
+let systemValues = [4, 3];
+let systemTroops = 0;
+
+troopCalculate(playerTroops, territories, systemValues, systemTroops);
