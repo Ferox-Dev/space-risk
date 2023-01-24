98bb28c633d82e2348abc7b29f6b9ebbfc72228f
Ferox <Feroxtheheist@protonmail.com>
2023-01-24 23:10:36 +0000

    Server Push app

---
+++ app.cjs
@@ -0,0 +1,25 @@
+const express = require('express')
+const app = express();
+const serv = require('http').Server(app);
+
+let SOCKET_LIST = [];
+let PLAYER_LIST = [];
+
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + '/client/index.html');
+});
+app.use('/client', express.static(__dirname + '/client'));
+serv.listen(4000);
+console.log("srvstrt")
+
+const io = require("socket.io")(serv, {});
+io.sockets.on('connection', function (socket) {
+
+    SOCKET_LIST[socket.id] = socket;
+
+    socket.on('disconnect', function () {
+        delete SOCKET_LIST[socket.id]
+    })
+
+})
+
