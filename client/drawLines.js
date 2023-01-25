06aba49659335ee34929ccad77afbb01914eede1
Ferox <Feroxtheheist@protonmail.com>
2023-01-25 19:27:12 +0000

    Shift files

--- client/drawLines.js
+++ client/drawLines.js
@@ -1,27 +1,27 @@
-import { planetsArray } from '../Server/index.js';
+import { planetsArray } from './js/index.js';
 
 let tempClick;
 
 export default function drawLines(planet, neighbours) {
-    
+
     let canvas = document.getElementById('canvas')
     let ctx = canvas.getContext('2d');
-    
+
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     let planetFind = planetsArray.find(item => item.planet == planet)
 
-    if(tempClick != planet) {
-        let x1 = (document.getElementById(planet).width/2)+(parseInt(document.getElementById(planet).style.left, 10));
-        let y1 = (document.getElementById(planet).height/2)+(parseInt(document.getElementById(planet).style.top, 10));
+    if (tempClick != planet) {
+        let x1 = (document.getElementById(planet).width / 2) + (parseInt(document.getElementById(planet).style.left, 10));
+        let y1 = (document.getElementById(planet).height / 2) + (parseInt(document.getElementById(planet).style.top, 10));
 
 
-        for(let i = 0; i < neighbours.length; i++) {
+        for (let i = 0; i < neighbours.length; i++) {
 
             let neighbourFind = planetsArray.find(item => item.planet == neighbours[i])
-            
-            if(planetFind.claimed != neighbourFind.claimed) {
-                let x2 = (document.getElementById(neighbours[i]).width/2)+(parseInt(document.getElementById(neighbours[i]).style.left, 10));
-                let y2 = (document.getElementById(neighbours[i]).height/2)+(parseInt(document.getElementById(neighbours[i]).style.top, 10));
+
+            if (planetFind.claimed != neighbourFind.claimed) {
+                let x2 = (document.getElementById(neighbours[i]).width / 2) + (parseInt(document.getElementById(neighbours[i]).style.left, 10));
+                let y2 = (document.getElementById(neighbours[i]).height / 2) + (parseInt(document.getElementById(neighbours[i]).style.top, 10));
 
                 ctx.beginPath();
                 ctx.moveTo(x1, y1);
