daa3782c3919e24cc5946493b2c87d9b704739c5
PatrickHacault <patrickhacault893@gmail.com>
2023-01-31 14:59:45 -0600

    troop calculation stuff

--- client/js/TroopCalculator.js
+++ client/js/TroopCalculator.js
@@ -1,13 +1,12 @@
-export default function troopCalculate(playerTroops, planets, systemValues, systemTroops) {
+export default function troopCalculate(player, planets, systemValues, systemTroops) {
    
 
     for(let i = 0; i < systemValues.length; i++) {
         systemTroops += systemValues[i];
     }
     
-    playerTroops += ((Math.floor(planets/3))+systemTroops+3);
-    console.log(playerTroops);
-    
+    player.troops += ((Math.floor(planets/3))+systemTroops+3);
+    return player.troops;
 }
 
 
